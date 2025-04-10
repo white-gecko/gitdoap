9d29444f413935cada5b5976ea36ffdc73439ce7
Natanael Arndt <arndtn@gmail.com>
2025-04-10 15:09:04 +0200

    Make it a function

--- gitdoap.py
+++ gitdoap.py
@@ -9,39 +9,53 @@ SIOC = Namespace("http://rdfs.org/sioc/ns#")
 # Semantically-Interlinked Online Developer Communities
 SIODC = Namespace("https://siodc.example.org/#")
 
-base_url = "https://github.com/"
-repo_url = "https://github.com/PyGithub/PyGithub"
-repo_name = repo_url[len(base_url):]
+def githubdoap(repo_url, base_url = "https://github.com/"):
+    # Normalize the repo_url
+    repo_url = repo_url.removesuffix(".git")
+    repo_url = repo_url.removeprefix("https://")
+    repo_url = repo_url.removeprefix("http://")
+    repo_url = f"https://{repo_url}"
+    repo_name = repo_url.removeprefix(base_url)
 
-gh = Github()
-repo = gh.get_repo(repo_name)
+    gh = Github()
+    repo = gh.get_repo(repo_name)
 
-g = Graph()
-repo_resource = g.resource(repo_url)
-repo_resource.add(RDF.type, DOAP.Project)
-repo_resource.add(DOAP.description, Literal(repo.description))
-repo_resource.add(DOAP.developer, URIRef(repo.owner.html_url))
-repo_resource.add(DOAP.homepage, URIRef(repo.homepage))
-repo_resource.add(DOAP.repository, URIRef(repo.git_url))
-repo_resource.add(DOAP.repository, URIRef(repo.ssh_url))
-repo_resource.add(DOAP.created, Literal(repo.created_at))
-repo_resource.add(DCT.date, Literal(repo.updated_at))
-repo_resource.add(DOAP.license, URIRef(repo.license.url))
-repo_resource.add(SIODC.num_stars, Literal(repo.stargazers_count))
-repo_resource.add(SIODC.num_watchers, Literal(repo.subscribers_count))
-if repo.issues_url:
-    repo_resource.add(SIODC.issue_tracker, URIRef(repo_url + "/issues"))
+    g = Graph()
+    repo_resource = g.resource(repo_url)
+    repo_resource.add(RDF.type, DOAP.Project)
+    repo_resource.add(DOAP.description, Literal(repo.description))
+    repo_resource.add(DOAP.developer, URIRef(repo.owner.html_url))
+    repo_resource.add(DOAP.homepage, URIRef(repo.homepage))
+    repo_resource.add(DOAP.repository, URIRef(repo.clone_url))
+    repo_resource.add(DOAP.repository, URIRef(repo.svn_url))
+    repo_resource.add(DOAP.created, Literal(repo.created_at))
+    repo_resource.add(DCT.date, Literal(repo.updated_at))
+    repo_resource.add(DOAP.license, URIRef(repo.license.url))
+    repo_resource.add(SIODC.num_stars, Literal(repo.stargazers_count))
+    repo_resource.add(SIODC.num_watchers, Literal(repo.subscribers_count))
+    if repo.issues_url:
+        repo_resource.add(SIODC.issue_tracker, URIRef(repo_url + "/issues"))
 
-developer_resource = g.resource(URIRef(repo.owner.html_url))
-# The typing is not yet cool, since doap:developer will make it a foaf:Person anyhow
-developer_resource.add(RDF.type, FOAF.Agent)
-if repo.owner.type == "Organization":
-    developer_resource.add(RDF.type, FOAF.Organization)
-if repo.owner.type == "User":
-    developer_resource.add(RDF.type, SIOC.User)
+    developer_resource = g.resource(URIRef(repo.owner.html_url))
+    # The typing is not yet cool, since doap:developer will make it a foaf:Person anyhow
+    developer_resource.add(RDF.type, FOAF.Agent)
+    if repo.owner.type == "Organization":
+        developer_resource.add(RDF.type, FOAF.Organization)
+    if repo.owner.type == "User":
+        developer_resource.add(RDF.type, SIOC.User)
 
-license_resource = g.resource(URIRef(repo.license.url))
-license_resource.add(RDFS.label, Literal(repo.license.name))
+    license_resource = g.resource(URIRef(repo.license.url))
+    license_resource.add(RDFS.label, Literal(repo.license.name))
 
-print(g.serialize(format="text/turtle"))
-# repo_resource.add(DOAP.created, Literal(repo.updated_at))
+    git_repo_resource = g.resource(URIRef(repo.git_url))
+    # The typing is not yet cool, since doap:developer will make it a foaf:Person anyhow
+    git_repo_resource.add(RDF.type, DOAP.GitRepository)
+    git_repo_resource.add(DOAP.repository, URIRef(repo.clone_url))
+    git_repo_resource.add(DOAP.repository, URIRef(repo.ssh_url))
+
+    svn_repo_resource = g.resource(URIRef(repo.svn_url))
+    # The typing is not yet cool, since doap:developer will make it a foaf:Person anyhow
+    svn_repo_resource.add(RDF.type, DOAP.SVNRepository)
+    svn_repo_resource.add(DOAP.repository, URIRef(repo.svn_url))
+
+    return g
