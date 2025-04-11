6449620b30a44a081a181b4f10e813d7d0a5d1a4
Natanael Arndt <arndtn@gmail.com>
2025-04-11 14:26:36 +0200

    Read Token from env

--- gitdoap.py
+++ gitdoap.py
@@ -1,7 +1,10 @@
 from rdflib import Graph, URIRef, Literal, RDF, RDFS, DOAP, DCTERMS as DCT, FOAF
 from rdflib.namespace import Namespace
 from github import Github
+from github.Auth import Token
+from github.GithubException import UnknownObjectException
 from urllib.parse import urlparse
+import os
 
 # Semantically-Interlinked Online Communities
 # https://www.w3.org/submissions/sioc-spec/
@@ -19,31 +22,57 @@ def doapit(repo_url: str) -> Graph | None:
 
 def normalize_github(repo_url: str, base_url: str = "https://github.com/") -> tuple[str, str]:
     """Normalize the repo_url an return it together with the repository name."""
+    repo_url = repo_url.removesuffix("/")
     repo_url = repo_url.removesuffix(".git")
     repo_url = repo_url.removeprefix("https://")
     repo_url = repo_url.removeprefix("http://")
     repo_url = f"https://{repo_url}"
-    repo_name = repo_url.removeprefix(base_url)
+    parsed = urlparse(repo_url)
+    repo_name = None
+    if len(parsed.path.split("/")) == 3:
+        repo_name = parsed.path.removeprefix("/")
     return repo_url, repo_name
 
 def github_doap(repo_url: str, base_url: str = "https://github.com/") -> Graph:
     """Construct a description from based on the results from the GitHub API."""
     repo_url, repo_name = normalize_github(repo_url, base_url)
 
-    gh = Github()
-    repo = gh.get_repo(repo_name)
+    if not repo_name:
+        return None
+
+    auth = None
+    if 'GH_TOKEN' in os.environ:
+        auth = Token(os.environ['GH_TOKEN'])
+    gh = Github(auth=auth)
+    try:
+        repo = gh.get_repo(repo_name)
+    except UnknownObjectException as e:
+        print(f"Error with {repo_name} ({repo_url}): {e}")
+        return None
 
     g = Graph()
     repo_resource = g.resource(repo_url)
+
+    if repo.full_name != repo_name:
+        """Redirect detected"""
+        repo_url = base_url + repo.full_name
+        new_repo_resource = g.resource(repo_url)
+        repo_resource.add(RDFS.seeAlso, new_repo_resource)
+        repo_resource = new_repo_resource
+
     repo_resource.add(RDF.type, DOAP.Project)
     repo_resource.add(DOAP.description, Literal(repo.description))
     repo_resource.add(DOAP.developer, URIRef(repo.owner.html_url))
-    repo_resource.add(DOAP.homepage, URIRef(repo.homepage))
+    if repo.homepage:
+        repo_resource.add(DOAP.homepage, URIRef(repo.homepage))
     repo_resource.add(DOAP.repository, URIRef(repo.clone_url))
     repo_resource.add(DOAP.repository, URIRef(repo.svn_url))
     repo_resource.add(DOAP.created, Literal(repo.created_at))
     repo_resource.add(DCT.date, Literal(repo.updated_at))
-    repo_resource.add(DOAP.license, URIRef(repo.license.url))
+    if repo.license and repo.license.url:
+        repo_resource.add(DOAP.license, URIRef(repo.license.url))
+        license_resource = g.resource(URIRef(repo.license.url))
+        license_resource.add(RDFS.label, Literal(repo.license.name))
     repo_resource.add(SIODC.num_stars, Literal(repo.stargazers_count))
     repo_resource.add(SIODC.num_watchers, Literal(repo.subscribers_count))
     if repo.issues_url:
@@ -57,8 +86,6 @@ def github_doap(repo_url: str, base_url: str = "https://github.com/") -> Graph:
     if repo.owner.type == "User":
         developer_resource.add(RDF.type, SIOC.User)
 
-    license_resource = g.resource(URIRef(repo.license.url))
-    license_resource.add(RDFS.label, Literal(repo.license.name))
 
     git_repo_resource = g.resource(URIRef(repo.git_url))
     # The typing is not yet cool, since doap:developer will make it a foaf:Person anyhow
